558cfb18313174a8007b8c41558232c8f95537ad
Maulana Zain <maulanadezain@gmail.com>
2023-04-14 21:36:59 +0700

    feat:refactor category response

--- internal/app/schema/category_schema.go
+++ internal/app/schema/category_schema.go
@@ -7,6 +7,6 @@ type GetCategoryResp struct {
 }
 
 type CreateCategoryReq struct {
-	Name        string
-	Description string
+	Name        string `validate:"required"`
+	Description string `validate:"required"`
 }
